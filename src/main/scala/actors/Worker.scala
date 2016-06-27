ec9788261fd3debc4350c36b2970433ec575b52d
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2016-06-27 16:48:52 +0200

    Refactor Worker using FSM (finite state machine) trait

--- src/main/scala/actors/Worker.scala
+++ src/main/scala/actors/Worker.scala
@@ -5,12 +5,15 @@ import java.util.UUID
 import actors.Worker.messages._
 import actors.WorkManager.answers.WorkIsReady
 import actors.WorkManager.messages.{RequestWork, WorkIsDone}
+import actors.Worker._
 import akka.actor.SupervisorStrategy.{Restart, Stop}
 import akka.actor._
 import models.core._
 import services.ProcessedImage
 
 import scala.concurrent.duration._
+
+import scala.language.existentials
 
 object Worker {
   val supervisorStrategy = OneForOneStrategy() {
@@ -20,6 +23,16 @@ object Worker {
   def props(workManager: ActorRef, registerInterval: FiniteDuration = 5.seconds) = {
     Props(new Worker(workManager, registerInterval))
   }
+
+  sealed trait State
+  case object Idle extends State
+  case object Working extends State
+  case object WaitingForWorkIsDoneAck extends State
+
+  sealed trait Data
+  case object NoWorkData extends Data
+  case class WorkData(currentWork: Work[_]) extends Data
+  case class ResultData[Result](currentResult: Result, workId: WorkId) extends Data
 
   object messages {
     case class WorkCompleted[Result](result: Result)
@@ -31,62 +44,59 @@ object Worker {
 }
 
 class Worker(val workManager: ActorRef, val registerInterval: FiniteDuration)
-  extends Actor with ActorLogging with WorkerRegistering with WorkExecutorChild {
+  extends FSM[State, Data] with ActorLogging with WorkerRegistering with WorkExecutorChild {
 
   lazy val workerId = WorkerId(UUID.randomUUID().toString)
 
-  private var currentWork: Option[Work[_]] = None
-  def currentWorkId: WorkId = currentWork match {
-    case Some(Work(workId, _)) =>
-      workId
-    case _ =>
-      throw new IllegalStateException("No current work")
-  }
+  startWith(Idle, NoWorkData)
 
-  override def receive = idle
-
-  def idle: Receive = {
-    case WorkIsReady =>
+  when(Idle) {
+    case Event(WorkIsReady, data) =>
       sendToWorkManager(RequestWork(workerId))
+      stay using data
 
 
-    case w @ Work(workId, workData) =>
+    case Event(w @ Work(workId, workData), NoWorkData) =>
       log.info(s"Worker ${workerId.value} received new work: ${workId.value}")
-      currentWork = Some(w)
       workExecutor ! workData
-      context.become(working)
+      goto(Working) using WorkData(w)
   }
 
-  def working: Receive = {
-    case WorkCompleted(result: ProcessedImage) =>
+  when(Working) {
+    case Event(WorkCompleted(result: ProcessedImage), WorkData(currentWork)) =>
+      val currentWorkId = currentWork.workId
       log.info(s"Work $currentWorkId is complete. Result: $result")
       sendToWorkManager(WorkIsDone(workerId, currentWorkId, result))
-      context.setReceiveTimeout(5.seconds)
-      context.become(waitForWorkIsDoneAck(result))
+      goto(WaitingForWorkIsDoneAck) using ResultData(result, currentWorkId)
 
-    case a =>
+    case Event(a, data) =>
       log.error(s"Worker ${workerId.value} received $a while working")
+      stay using data
   }
 
-  def waitForWorkIsDoneAck(result: ProcessedImage): Receive = {
-    case WorkManager.answers.WorkIsDoneAck(workId) if workId == currentWorkId =>
+  when(WaitingForWorkIsDoneAck, stateTimeout = 5.seconds) {
+    case Event(WorkManager.answers.WorkIsDoneAck(workId), data @ ResultData(_, resultWorkId))
+      if workId == resultWorkId =>
+
       sendToWorkManager(RequestWork(workerId))
-      context.setReceiveTimeout(Duration.Undefined)
-      context.become(idle)
+      goto(Idle) using NoWorkData
 
-    case ReceiveTimeout =>
+    case Event(StateTimeout, data @ ResultData(result, resultWorkId)) =>
       log.info("No ack from manager, retrying")
-      sendToWorkManager(WorkIsDone(workerId, currentWorkId, result))
+      sendToWorkManager(WorkIsDone(workerId, resultWorkId, result))
+      stay using data
   }
 
-  override def unhandled(message: Any): Unit = message match {
-    case Terminated(`workExecutor`) =>
-      context.stop(self)
+  whenUnhandled {
+    case Event(Terminated(`workExecutor`), data) =>
+      stop()
 
-    case WorkIsReady =>
+    case Event(WorkIsReady, data) =>
+      stay using data
 
-    case _ =>
-      log.warning(s"Worker ${workerId.value} received unrecognized message: $message")
+    case Event(e, s) =>
+      log.warning(s"Worker ${workerId.value} received unrecognized message: $e in state: $stateName/$s")
+      stay using s
   }
 
   def sendToWorkManager(msg: Any): Unit = {
@@ -113,7 +123,6 @@ trait WorkerRegistering { self: Worker =>
 
 trait WorkExecutorChild { self: Worker =>
   def workerId: WorkerId
-  def currentWorkId: WorkId
   def workManager: ActorRef
 
   val workExecutor = context.watch(context.actorOf(WorkExecutor.props, "exec"))
@@ -122,8 +131,11 @@ trait WorkExecutorChild { self: Worker =>
     case _: ActorInitializationException => Stop
     case _: DeathPactException           => Stop
     case _: Exception =>
+      val currentWorkId = stateData match {
+        case WorkData(Work(workId, _)) => workId
+        case _ => WorkId("no current work")
+      }
       workManager ! WorkManager.messages.WorkFailed(workerId, currentWorkId)
-      context.become(idle)
       Restart
   }
 }
